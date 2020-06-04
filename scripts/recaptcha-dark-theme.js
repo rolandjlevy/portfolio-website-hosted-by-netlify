3bf5cc229cc9a91b20dd33681869d56f6ab1c2a5
Roland Levy <rolandjlevy@gmail.com>
2020-06-04 14:42:38 +0000

    Trying to set recaptcha attribute "data-theme" to "dark"

--- scripts/recaptcha-dark-theme.js
+++ scripts/recaptcha-dark-theme.js
@@ -2,17 +2,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   const iframe = document.querySelector('.g-recaptcha iframe');
 
-  let content = (iframe.contentWindow || iframe.contentDocument);
-  if (content.document) {
-    content = content.document;
-  }
+  const recaptcha = document.querySelector('.g-recaptcha');
 
-  const cssFile = '/styles/recaptcha-dark-theme.css'
-  let file = content.createElement("link");
-  file.setAttribute("rel", "stylesheet");
-  file.setAttribute("type", "text/css");
-  file.setAttribute("href", cssFile);
-  content.head.appendChild(file);
+  console.log({recaptcha});
+
+  recaptcha.setAttribute("data-theme", "dark");
+
+  // let content = (iframe.contentWindow || iframe.contentDocument);
+  // if (content.document) {
+  //   content = content.document;
+  // }
+
+  // const cssFile = '/styles/recaptcha-dark-theme.css';
+  // let file = content.createElement("link");
+  // file.setAttribute("rel", "stylesheet");
+  // file.setAttribute("type", "text/css");
+  // file.setAttribute("href", cssFile);
+  // content.head.appendChild(file);
   
   // content.body.innerHTML = content.body.innerHTML +
   // `<style>

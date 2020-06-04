41fef3ea0dc38101e07a52854e05fa9d006d4c47
Roland Levy <rolandjlevy@gmail.com>
2020-06-04 14:19:47 +0000

    trying to inject css file recaptcha-dark-theme.css

--- scripts/recaptcha-dark-theme.js
+++ scripts/recaptcha-dark-theme.js
@@ -1,28 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
   const iframe = document.querySelector('.g-recaptcha iframe');
+  
   let content = (iframe.contentWindow || iframe.contentDocument);
   if (content.document) {
     content = content.document;
   }
-  content.body.innerHTML = content.body.innerHTML +
-  `<style>
 
-    .rc-anchor-light.rc-anchor-normal {
-      border: 1px solid #666 !important;
-    }
+  const cssFile = '/styles/recaptcha-dark-theme.css'
+  let file = content.createElement("link");
+  file.setAttribute("rel", "stylesheet");
+  file.setAttribute("type", "text/css");
+  file.setAttribute("href", cssFile);
+  content.head.appendChild(file);
+  
+  // content.body.innerHTML = content.body.innerHTML +
+  // `<style>
 
-    .rc-anchor-light {
-      background: #333 !important;
-      color: #fff !important;
-    }
-
-    .recaptcha-checkbox-border {
-      background-color: #333 !important;
-      border: 2px solid #666 !important;
-    }
-    
-  </style>
-  `;
+  // </style>
+  // `;
 
 });

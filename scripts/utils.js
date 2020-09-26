6748297f98a1398a73e132b96abd464b5afef704
Roland Levy <rolandjlevy@gmail.com>
2020-09-26 08:32:39 +0000

    Adding CSS for lighter theme

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,26 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+// collapse burger menu panel in mobile view 
 
-  // collapse burger menu panel in mobile view 
+const mediaQueryMobile = 600;
+const burgerCheckBox = document.querySelector('.burger-checkbox-class');
+const contactForm = document.querySelector('#contact-form');
+const body = document.querySelector('body');
 
-  const mediaQueryMobile = 600;
-  const burgerCheckBox = document.querySelector('.burger-checkbox-class');
-  const body = document.querySelector('body');
-
-  const myObserver = new ResizeObserver(entries => {
-    entries.forEach(entry => {
-      const contentWidth = entry.contentRect.width;
-      if (contentWidth > mediaQueryMobile && burgerCheckBox.checked) {
-      burgerCheckBox.checked = false;
-      }
-    });
+const myObserver = new ResizeObserver(entries => {
+  entries.forEach(entry => {
+    const contentWidth = entry.contentRect.width;
+    if (contentWidth > mediaQueryMobile && burgerCheckBox.checked) {
+    burgerCheckBox.checked = false;
+    }
   });
-  
-  myObserver.observe(body);
-
-  // change recaptcha to dark by setting the data-theme attribute
-  const recaptcha = document.querySelector('.g-recaptcha');
-  if (recaptcha) {
-    recaptcha.setAttribute("data-theme", "dark");
-  }
-
 });
+
+myObserver.observe(body);
+
+// change recaptcha to dark by setting the data-theme attribute
+const recaptcha = document.querySelector('.g-recaptcha');
+if (recaptcha) {
+  recaptcha.setAttribute("data-theme", "dark");
+}

7f779daad58f6d0a1b9cf3cfdfdb71d8bc4ccc3c
Roland Levy <rolandjlevy@gmail.com>
2020-09-27 12:22:54 +0000

    Added theme toggle button to nav and burger menu

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,6 +3,7 @@
 const html = document.querySelector('html');
 const body = document.querySelector('body');
 const themeButtons = document.querySelectorAll('.theme');
+const themeEmojis = document.querySelectorAll('label[for*="theme"] > .emoji');
 const mediaQueryMobile = 600;
 const burgerCheckBox = document.querySelector('.burger-checkbox-class');
 const contactForm = document.querySelector('#contact-form');
@@ -18,16 +19,25 @@ const myObserver = new ResizeObserver(entries => {
 
 myObserver.observe(body);
 
-console.log({themeButtons})
-
 themeButtons.forEach(item => {
   item.addEventListener('click', (e) => {
     const customTheme = html.dataset.theme === 'dark' ? 'light' : 'dark';
-    html.dataset.theme = customTheme
+    html.dataset.theme = customTheme;
+    invertEmojiColour(customTheme);
     // const customTheme = e.target.checked ? 'clean' : 'dark';
     // updateRecaptcha(customTheme);
   });
 });
+
+function invertEmojiColour(customTheme) {
+  themeEmojis.forEach(item => {
+    if (customTheme === 'light') {
+      item.classList.add('light');
+    } else {
+      item.classList.remove('light');
+    }
+  });
+}
 
 // change recaptcha to dark by setting the data-theme attribute
 
